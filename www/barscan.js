25257e7b3d90653e691cc14f2125bcce1fb063b8
angyperugini <angelo@equipnet.it>
2014-09-22 16:13:09 +0200

    Update barscan.js

--- www/barscan.js
+++ www/barscan.js
@@ -3,7 +3,7 @@ cordova.define("com.equipnet.tools.barcode", function(require, exports, module) 
 var cordova = require('cordova');
 
 var barcode = {
-	getImeiNumber : function(successCallback, errorCallback){
+	getScannedCode : function(successCallback, errorCallback){
 	cordova.exec(successCallback, errorCallback,'Barscan', 'getScannedCode', []);//ImeiPlugin ->stringa che identifica 											      //l'oggetto e il rispettivo metodo richiamato
 } 											//[] e relativi eventuali parametri
 };
