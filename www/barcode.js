f82d53f9a5533063443c8aee3f7c8588df8dc78b
angyperugini <angelo@equipnet.it>
2014-09-23 14:53:48 +0200

    Update and rename barscan.js to barcode.js

---
+++ www/barcode.js
@@ -0,0 +1,16 @@
+/** 
+ * 
+ * @return Instance of Barcode 
+ */ 
+var Barcode = function() {} 
+
+Barcode.prototype.read = function( successCallback, failureCallback ) { 
+    return cordova.exec(successCallback,failureCallback,'Barcode','read'); 
+}; 
+
+if(!window.plugins) { 
+    window.plugins = {}; 
+} 
+if (!window.plugins.Barcode) { 
+    window.plugins.Barcode = new Barcode(); 
+}
