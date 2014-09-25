86401e5d7ddeb79468a9d2c07b1eb1c219d01819
angyperugini <angelo@equipnet.it>
2014-09-25 10:15:32 +0200

    Update barcode.js

--- www/barcode.js
+++ www/barcode.js
@@ -1,8 +1,10 @@
 cordova.define("com.equipnet.plugin.barcode", function(require, exports, module) { 
-	
+
+var cordova = require('cordova');
+
 var barcode = {
 	read : function(successCallback, errorCallback){
-	cordova.exec(successCallback, errorCallback,'Barcode', 'read', []);//ImeiPlugin ->stringa che identifica 											      //l'oggetto e il rispettivo metodo richiamato
+	cordova.exec(successCallback, errorCallback,'barcode', 'read', []);//ImeiPlugin ->stringa che identifica 											      //l'oggetto e il rispettivo metodo richiamato
 } 											//[] e relativi eventuali parametri
 };
 
