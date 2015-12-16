f6ec762ce8106cb1d21c0a9d3189c77377da99cc
angyperugini <angelo@equipnet.it>
2015-12-16 10:59:42 +0100

    Update Barcode.java

--- src/android/Barcode.java
+++ src/android/Barcode.java
@@ -9,20 +9,44 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaWebView;
 
+
+
+
+
+
+
+import org.apache.cordova.PluginResult;
+
+import org.apache.cordova.PluginResult.Status;
+
 //importazioni da Android
 import android.content.Context;
 import android.hardware.barcode.Scanner;
 import android.telephony.TelephonyManager;
+
 import org.apache.cordova.CordovaPlugin;
 
-
 import android.util.Log; 
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.webkit.WebView;
 
 public class Barcode extends CordovaPlugin { 
              
         public static final String READ = "read"; 
         public static final String ME = "logging"; 
-   
+        
+//        public boolean onKey(WebView v, int keyCode, KeyEvent event) {
+//
+//            if(event.getKeyCode() == 211){
+//    			Scanner.Read(); 
+//            	return true;
+//
+//    		}
+//            return false;
+//        }
+  
 
         
         public void initialize(CordovaInterface cordova, CordovaWebView webview) {
@@ -34,19 +58,31 @@ public class Barcode extends CordovaPlugin {
     	}
         
          
-        public boolean execute(String action, JSONArray data, CallbackContext callbackContext)
+        public boolean execute(String action, JSONArray data, final CallbackContext callbackContext)
     			throws JSONException{
-            Log.d(ME, "execute " + action); 
-            if (action.equalsIgnoreCase(READ)) {  
-            	Scanner.Read();
-            	callbackContext.success(Scanner.ReadSCAAuto());
-                Log.v(ME, "stringacodice " + action);
-            	return true;
-            } 
-            return false; 
+        	  PluginResult result = null;
+              if (action.equalsIgnoreCase(READ)) {
+                  this.cordova.getActivity().runOnUiThread(new Runnable() {
+                      public void run() {
+                          callbackContext.success(Scanner.ReadSCAAuto()); 
+                      }
+                  });
+                  result = new PluginResult(Status.OK);
+                  
+              }
+//            Log.d(ME, "execute " + action); 
+//            if (action.equalsIgnoreCase(READ)) {  
+//            	Scanner.Read();
+//            	callbackContext.success(Scanner.ReadSCAAuto());
+//                Log.v(ME, "Codice Letto " + action);
+//            	return true;
+//            } 
+//            return false; 
+			return false;
         } 
-         
         public static void sendJavascript( JSONObject data ) { 
             
         } 
+       
+
 }
